575cc722ed9cd961a9ac2c4eabf2ca0637612a2d
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-07-22 20:59:31 -0400

    ✨ Adding another endpoint to manage users

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export { createNewCategory, retrieveCategoriesByHouseholdId } from './categories';
 export { addNewExpenses } from './expenses';
 export { getHouseholdsData } from './households';
-export { addNewUserAndHousehold, getUserDetails } from './users';
+export { addNewUserAndHousehold, addNewUserToHousehold, getUserDetails } from './users';
